321d8fd54a3aa05f270e2080c650825a5a77d01d
Brady Wied <brady@bswtechconsulting.com>
2016-03-11 07:20:16 -0700

    Make this more efficient

--- spec/rails_assets.rb
+++ spec/rails_assets.rb
@@ -1,4 +1,5 @@
-# Need a Phantom polyfill, there is a docs and js path, order is not guaranteed, so just append both
-RailsAssetsEs5Shim.load_paths.each { |p| Opal.append_path p }
-RailsAssetsJquery.load_paths.each { |p| Opal.append_path p }
-RailsAssetsReact.load_paths.each { |p| Opal.append_path p }
+APPEND = [RailsAssetsEs5Shim, RailsAssetsJquery, RailsAssetsReact]
+
+APPEND.each do |rails_asset|
+  rails_asset.load_paths.each { |p| Opal.append_path p }
+end
